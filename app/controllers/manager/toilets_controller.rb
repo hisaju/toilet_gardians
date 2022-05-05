fa9a597a9dfa4838c2c30fa42fe8cc8b88b52236
hisaju <hisaju@gmail.com>
2022-05-05 17:20:52 +0900

    トイレ登録追加

--- app/controllers/manager/toilets_controller.rb
+++ app/controllers/manager/toilets_controller.rb
@@ -1,4 +1,19 @@
 class Manager::ToiletsController < Manager::BaseController
   def index
+    @toilets = current_user.toilets.order(created_at: :desc)
+    @toilet = current_user.toilets.new
   end
+
+  def create
+    @toilet = current_user.toilets.new(toilet_params)
+    if @toilet.save
+      @toilet = current_user.toilets.new
+    end
+    @toilets = current_user.toilets.order(created_at: :desc)
+  end
+
+  def toilet_params
+    params.require(:toilet).permit(:name)
+  end
+
 end
