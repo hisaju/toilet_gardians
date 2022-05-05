d28ea3524300dac6da6957c9f672fc1c76edb488
hisaju <hisaju@gmail.com>
2022-05-05 17:29:00 +0900

    トイレ生成時にUUID付与

--- app/models/toilet.rb
+++ app/models/toilet.rb
@@ -2,4 +2,14 @@ class Toilet < ApplicationRecord
   belongs_to :user
 
   validates :name, presence: true
+
+  before_save :set_uuid
+
+  def set_uuid
+    uuid = SecureRandom.hex(13)
+    while (Toilet.find_by(uuid: uuid)) do
+      uuid = SecureRandom.hex(13)
+    end
+    self.uuid = uuid
+  end
 end
